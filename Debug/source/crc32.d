e41b9e044326f402d25f8c604aee74dd006b33c4
parkerm1417 <parkerm1417@gmail.com>
2025-10-31 13:46:03 -0500

    Remove debug files and update gitignore

--- Debug/source/crc32.d
+++
@@ -1,2 +0,0 @@
-source/crc32.o source/crc32.d: ../source/crc32.c ../source/crc32.h
-../source/crc32.h:
--- Debug/startup/startup_mkl02z4.d
+++
@@ -1,2 +0,0 @@
-startup/startup_mkl02z4.o startup/startup_mkl02z4.d: \
- ../startup/startup_mkl02z4.c
